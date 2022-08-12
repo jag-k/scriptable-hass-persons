6f98eb154cc76cb908d9bd7da2b963f6235d0c44
Jag_k <jag.konon@gmail.com>
2022-08-12 15:50:27 +0300

    :arrow_up: Update dependency versions

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
-import scriptableBundle from "rollup-plugin-scriptable";
+import scriptableBundle from "@jag-k/rollup-plugin-scriptable";
 import * as config from "./config.json";
 import copy from 'rollup-plugin-copy';
 
