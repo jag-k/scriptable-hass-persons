5374c556a67e38889e6dbed119970aeae74db1e2
Jag_k <jag.konon@gmail.com>
2022-08-14 04:25:18 +0300

    :memo: Add display all size of widgets

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,19 @@ if (config.runsInWidget) {
   Script.setWidget(await widget())
 } else if (config.runsInApp) {
   if (loggedIn) {
+    // Showing all widgets
+    c.displayMode('small');
+    const widgetSmall = await widget();
+    await widgetSmall.presentSmall();
     c.displayMode('medium');
-    const wM = await widget();
-    await wM.presentMedium();
+    const widgetMedium = await widget();
+    await widgetMedium.presentMedium();
+    c.displayMode('large');
+    const widgetLarge = await widget();
+    await widgetLarge.presentLarge();
+    c.displayMode('extraLarge');
+    const widgetExtraLarge = await widget();
+    await widgetExtraLarge.presentExtraLarge();
   } else {
     await loginMenu()
   }
